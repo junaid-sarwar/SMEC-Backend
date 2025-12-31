1da5125a563ce683d101a9fc1b978854512e260e
junaid-sarwar <junaidsarwariqbal@gmail.com>
2025-12-31 13:06:58 +0500

    fixing cloudinary

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,7 +1,9 @@
 // config/cloudinary.js
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('cloudinary');
 const multer = require('multer');
+
+// 👇 EXPLICIT import from the object you just logged
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -10,13 +12,13 @@ cloudinary.config({
 });
 
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
+  cloudinary,
   params: {
     folder: 'SMEC_Events',
     allowed_formats: ['jpg', 'png', 'jpeg'],
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage });
 
 module.exports = { cloudinary, upload };
